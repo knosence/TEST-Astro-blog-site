d67a6d5957540dc5522427e4594be8fdb8d560bf
NaDario Seays <knosence@pop-os.localdomain>
2023-12-19 19:50:57 -0500

    attempting to fix bug in vercel

--- src/components/PostLike.tsx
+++ src/components/PostLike.tsx
@@ -1,7 +1,7 @@
 export default function PostLike() {
 	return (
 		<button 
-      onClick={() => alert('Hello, island!')} 
+      onclick={() => alert('Hello, island!')} 
       className="px-3 py-1 rounded-md bg-blue-500 text-white">
 			Like
 		</button>
