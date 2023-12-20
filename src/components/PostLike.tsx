1390608ddb0661bd82b673376c1ca8a9f42519b5
NaDario Seays <knosence@pop-os.localdomain>
2023-12-19 19:52:28 -0500

    Meh

--- src/components/PostLike.tsx
+++ src/components/PostLike.tsx
@@ -1,7 +1,7 @@
 export default function PostLike() {
 	return (
 		<button 
-      onclick={() => alert('Hello, island!')} 
+      onClick={() => alert('Hello, island!')} 
       className="px-3 py-1 rounded-md bg-blue-500 text-white">
 			Like
 		</button>
