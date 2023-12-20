9aae8c70c901dfe0f9a896cb39e1639819a3599f
NaDario Seays <knosence@pop-os.localdomain>
2023-12-19 19:28:41 -0500

    addes react like button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://replaceme.com',
   integrations: [tailwind(), sitemap({
-    filter: (page) => !/https:\/\/replaceme\.com\/blog\/[0-9]+/.test(page),
-    customPages: [
-      'https://replaceme.com/discord',
-      'https://replaceme.com/twitter',
-    ],
+    filter: page => !/https:\/\/replaceme\.com\/blog\/[0-9]+/.test(page),
+    customPages: ['https://replaceme.com/discord', 'https://replaceme.com/twitter'],
     changefreq: 'weekly',
     lastmod: new Date(),
     priority: 0.85,
-    serialize: (item) => {
+    serialize: item => {
       if (item.url.at(-1) === '/') {
         item.url = item.url.slice(0, -1);
       }
@@ -26,10 +24,8 @@ export default defineConfig({
       locales: {
         en: 'en-US',
         es: 'es-ES',
-        fr: 'fr-CA',
-      },
-    },
-    
-  }),
-  ],
+        fr: 'fr-CA'
+      }
+    }
+  }), react()]
 });
---
+++ src/components/PostLike.tsx
@@ -0,0 +1,9 @@
+export default function PostLike() {
+	return (
+		<button 
+      onClick={() => alert('Hello, island!')} 
+      className="px-3 py-1 rounded-md bg-blue-500 text-white">
+			Like
+		</button>
+	);
+}
