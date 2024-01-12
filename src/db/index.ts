7605b68ea7e21b6707861a7f886eca6e77848794
knosence <nadario.seays@gmail.com>
2024-01-12 01:08:18 -0500

    Bug fix: drizzle wouldnt run, typo in drizzle config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,13 @@
 import Database from 'better-sqlite3';
-import { drizzle, type BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
-
-import {migrate} from 'drizzle-orm/better-sqlite3/migrator';
+import {
+  drizzle,
+  type BetterSQLite3Database,
+} from 'drizzle-orm/better-sqlite3';
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 
 const sqlite = new Database(process.env.DATABASE_URL || './local.db');
 
 export const db: BetterSQLite3Database = drizzle(sqlite, { logger: true });
+migrate(db, { migrationsFolder: 'src/db/migrations' });
 
-migrate(db, {migrationsFolder: 'src/db/migrations'})
-
-export {likes} from './schema';
+export { likes } from './schema';
